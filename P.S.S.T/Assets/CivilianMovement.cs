07f13f6c066ab1d6e81fe795572874f2630c00cd
TKGotHub <31891399+TKGotHub@users.noreply.github.com>
2018-11-13 18:53:40 +0000

    PSST Game (AI)
    PSST game with Moving AI.

---
+++ P.S.S.T/Assets/CivilianMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class CivilianMovement : MonoBehaviour
+{
+    public Transform Point1;
+    public Transform Point2;
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        nav.SetDestination(Point1.position);
+    }
+
+
+    void Update()
+    {
+        {
+          
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Point1")
+        {
+            nav.SetDestination(Point2.position);
+        }
+        if (other.name == "Point2")
+        {
+            nav.SetDestination(Point1.position);
+        }
+    }
+}
